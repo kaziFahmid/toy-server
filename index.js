df04311e1694fd195625d2eb43823dba4e1019cd
kaziFahmid <nirjhorislam42@gmail.com>
2023-05-18 22:09:28 +0600

    delete added

--- index.js
+++ index.js
@@ -79,7 +79,15 @@ async function run() {
 
 
   })
+  app.delete('/addedtoys/:id',async (req, res) => {
 
+    const id =req.params.id
+    const query={_id: new ObjectId(id)}
+    const result=await addedtoys.deleteOne(query)
+    res.send(result)
+
+
+  })
 
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
